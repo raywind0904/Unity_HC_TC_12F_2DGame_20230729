3b84e122988420770850d017d5426b713179516b
KID <50666207+KID421@users.noreply.github.com>
2023-08-26 16:02:25 +0800

    Update 完成場景系統

---
+++ Assets/Scripts/SceneSystem.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSystem : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("遊戲場景");
+    }
+
+    public void QuitGame()
+    {
+        print("<color=yellow>離開遊戲</color>");
+        Application.Quit();
+    }
+}
