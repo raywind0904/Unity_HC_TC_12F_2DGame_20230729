5818c9098e08d09b03c9c9e821508c4bd79a86b5
KID <50666207+KID421@users.noreply.github.com>
2023-09-02 20:34:34 +0800

    Update 完成敵人追蹤

---
+++ Assets/Scripts/EnemySystem.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class EnemySystem : MonoBehaviour
+{
+    [Header("敵人資料")]
+    public DataEnemy data;
+
+    public Transform playerPoint;
+
+    private void Awake()
+    {
+        playerPoint = GameObject.Find("黑貓").transform;
+    }
+
+    private void Update()
+    {
+        TrackPlayer();
+        Flip();
+    }
+
+    private void TrackPlayer()
+    {
+        // 如果 敵人 與 玩家 距離 小於 3 就 跳出
+        if (Vector2.Distance(transform.position, playerPoint.position) < 3) return;
+        
+        transform.position = Vector2.MoveTowards(transform.position, playerPoint.position, data.speed * Time.deltaTime);
+    }
+
+    private void Flip()
+    {
+        if (transform.position.x > playerPoint.position.x) transform.eulerAngles = Vector2.zero;
+        else if (transform.position.x < playerPoint.position.x) transform.eulerAngles = new Vector2(0, 180);
+    }
+}
