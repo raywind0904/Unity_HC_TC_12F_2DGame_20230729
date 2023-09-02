05b1b40a2629fdd35bfed3f07192539b80e282f7
KID <50666207+KID421@users.noreply.github.com>
2023-09-02 16:08:01 +0800

    Update 武器系統

---
+++ Assets/Scripts/WeaponSystem.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class WeaponSystem : MonoBehaviour
+{
+    [Header("生成武器間隔"), Range(0, 6)]
+    public float interval = 3;
+    [Header("武器預製物")]
+    public GameObject prefabWeapon;
+    [Header("武器發射力道")]
+    public Vector2 v2Power = new Vector2(0, 100);
+
+    private void Awake()
+    {
+        // SpawnWeapon();
+
+        // 重複呼叫 SpawnWeapon 間隔為 interval
+        InvokeRepeating("SpawnWeapon", 0, interval);
+    }
+
+    private void SpawnWeapon()
+    {
+        GameObject temp = Instantiate(prefabWeapon, transform.position, transform.rotation);
+        temp.GetComponent<Rigidbody2D>().AddForce(v2Power);
+    }
+}
